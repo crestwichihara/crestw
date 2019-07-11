430e60195dd80b9df66d99b236ad5ae5a2efeeb4
crestwichihara <52553995+crestwichihara@users.noreply.github.com>
2019-07-11 17:54:03 +0900

    201907111754

--- js/mail_mini.js
+++
@@ -1,5 +0,0 @@
-function secretaddressmini() {
-	var ma = String.fromCharCode(121-22,99+15,116-15,84+31,118-2,89+30,73-9,99+0,139-25,76+25,117-2,88+28,143-24,26+20,118-19,108+3,60-14,83+23,127-15);
-	var mt = String.fromCharCode(149-40,91+6,153-48,104+4,151-35,61+50,114-56);
-	document.write('<div class="quick_mail_f"><a href="'+ mt + ma + '"><div class="bg_mail_ico"></div></a></div>');
-}
